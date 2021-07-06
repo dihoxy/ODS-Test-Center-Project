4bb9f2a3fd5770e1493915eac9444e0882e38b2f
dihoxy <78871260+dihoxy@users.noreply.github.com>
2021-07-06 09:28:47 -0500

    Update ODS_Sql_Queries.sql
    Queries I've used to explore this dataset

--- ODS_Sql_Queries.sql
+++ ODS_Sql_Queries.sql
@@ -2,6 +2,20 @@
 
 --Average amount of time used per subject
 SELECT "subject", ROUND(AVG("actual_time")) AS "avg_time_used_per_exam"
-FROM public."Regular_Semester_Exams"
+  FROM public."Regular_Semester_Exams"
 GROUP BY("subject")
 ORDER BY AVG("actual_time") DESC;
+
+--Number of no shows per subject
+SELECT "subject", count("no_show") as "no show"
+  FROM public."Regular_Semester_Exams"
+    WHERE "no_show" = true		
+GROUP BY("subject")
+ORDER BY COUNT("no_show") DESC;
+
+--Number of cancelled exams per subject
+SELECT "subject", count("exam_cancelled") as "cancelled"
+  FROM public."Regular_Semester_Exams"
+	  WHERE "exam_cancelled" = true		
+GROUP BY("subject")
+ORDER BY COUNT("exam_cancelled") DESC;
