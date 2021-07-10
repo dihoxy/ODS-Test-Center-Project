fccc33b96a46ce4203484e9aee2df4c533d179bd
dihoxy <78871260+dihoxy@users.noreply.github.com>
2021-07-10 10:27:52 -0500

    Update ODS_Sql_Queries.sql

--- ODS_Sql_Queries.sql
+++ ODS_Sql_Queries.sql
@@ -14,7 +14,7 @@
 --SQL Queries
 
 --Average amount of time used per subject
-SELECT "subject", ROUND(AVG("actual_time")) AS "avg_time_used_per_exam"
+SELECT "subject", ROUND(AVG("actual_time")) AS "avg_time_used"
 FROM public."Regular_Semester_Exams"
 GROUP BY("subject")
 ORDER BY count("allotted_time") DESC;
