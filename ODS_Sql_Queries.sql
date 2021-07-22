d1b6c9e0b5cae2bf22d363eacc250e4b94da64bc
dihoxy <78871260+dihoxy@users.noreply.github.com>
2021-07-22 15:47:12 -0500

    Add files via upload

--- ODS_Sql_Queries.sql
+++ ODS_Sql_Queries.sql
@@ -1,30 +1,36 @@
 --Loading CSV With '/copy'
-\copy public."ods_exams"("index", "subject","section","exam_date","proctor","room_number","start_time","end_time","actual_start","actual_end","first_entered","fileUploaded","received_as_paper_copy","rescheduled","breaks_during_exams","extra_time_1.50x","extra_time_2.00x","makeup_acoommodation","noScantronExam","readerforExams","allotted_time","actual_time","exam_cancelled","no_show","requested_in_advance","name_of_day") FROM 'C:\Users\amvanslambrouck\Desktop\ODS Test Center Docs\ODS-Test-Center-Project-main\ODS-Test-Center-Project-dihoxy-version-2\postgres_export_ods_v03.csv' DELIMITER ','CSV HEADER;
+\copy public."ods_exams"("index", "subject","section","exam_date","proctor","room_number","start_time","end_time","actual_start","actual_end","first_entered","fileUploaded","received_as_paper_copy","rescheduled","breaks_during_exams","extra_time_1.50x","extra_time_2.00x","makeup_acoommodation","noScantronExam","readerforExams","allotted_time","actual_time","exam_cancelled","no_show","requested_in_advance","name_of_day") FROM 'C:\Users\amvanslambrouck\Desktop\ODS Test Center Docs\ODS-Test-Center-Project-main\ODS-Test-Center-Project-main\postgres_export_ods_v04.csv' DELIMITER ','CSV HEADER;
 
 --Creating Tables
 CREATE TABLE public."ods_exams"
-("index" FLOAT PRIMARY KEY, "subject" VARCHAR(10) NOT NULL,"section" FLOAT NOT NULL,"exam_date" DATE NOT NULL,
- "proctor" VARCHAR(35) NOT NULL,"room_number" VARCHAR(50) NOT NULL,"start_time" TIME NOT NULL,
- "end_time" TIME NOT NULL,"actual_start" TIME NOT NULL,"actual_end" TIME NOT NULL,"first_entered" DATE NOT NULL,
- "fileUploaded" BOOLEAN NOT NULL,"received_as_paper_copy" BOOLEAN NOT NULL,"rescheduled" FLOAT NOT NULL,
+("index" BIGINT PRIMARY KEY, "subject" VARCHAR(10) NOT NULL,"section" varchar(5) NOT NULL,"exam_date" DATE NOT NULL,
+ "proctor" VARCHAR(35) NOT NULL,"room_number" VARCHAR(50) NOT NULL,"start_time" TIMESTAMP NOT NULL,
+ "end_time" TIMESTAMP NOT NULL,"actual_start" TIMESTAMP NOT NULL,"actual_end" TIMESTAMP NOT NULL,"first_entered" TIMESTAMP NOT NULL,
+ "fileUploaded" BOOLEAN NOT NULL,"received_as_paper_copy" BOOLEAN NOT NULL,"rescheduled" BIGINT NOT NULL,
  "breaks_during_exams" BOOLEAN NOT NULL,"extra_time_1.50x" BOOLEAN NOT NULL,"extra_time_2.00x" BOOLEAN NOT NULL,
  "makeup_acoommodation" BOOLEAN NOT NULL,"noScantronExam" BOOLEAN NOT NULL,"readerforExams" BOOLEAN NOT NULL,
- "allotted_time" FLOAT NOT NULL,"actual_time" FLOAT,"exam_cancelled" BOOLEAN NOT NULL,"no_show" BOOLEAN NOT NULL,
- "days_requested_submitted_in_advance" FLOAT NOT NULL,"name_of_day" VARCHAR(10));
+ "allotted_time" BIGINT NOT NULL,"actual_time" FLOAT,"exam_cancelled" BOOLEAN NOT NULL,"no_show" BOOLEAN NOT NULL,
+ "requested_in_advance" BIGINT NOT NULL,"name_of_day" VARCHAR(10));
 
 
 
 
 --SQL Queries
+
+
+--Delete Test Case
+DELETE FROM ods_exams
+WHERE "subject" = 'TEST';
+
 --Average amount of time used per subject
 SELECT "subject", ROUND(AVG("actual_time")) AS "avg_time_used"
-FROM public."Regular_Semester_Exams"
+FROM public."ods_exams"
 GROUP BY("subject")
-ORDER BY count("allotted_time") DESC;
+ORDER BY count("actual_time") DESC;
 
 --Average Start Time and End Time for Evening Exams (Exams Past 4:45 p.m.)
 SELECT "subject", count("index"), avg("start_time"), avg("end_time")
-FROM public."Regular_Semester_Exams"
+FROM public."ods_exams"
 WHERE "start_time" > '16:45:00'
 GROUP BY ("subject")
 ORDER BY count("index") DESC;
