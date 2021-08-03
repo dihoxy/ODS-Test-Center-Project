07ed7deaec69849f0a2fe2b77488092e86e2e2a0
gtair31 <gtair31@gmail.com>
2021-08-03 06:56:46 -0500

    08/03/2021

--- ODS_Sql_Queries.sql
+++ ODS_Sql_Queries.sql
@@ -8,7 +8,7 @@
 
 ----+--Creating Tables and altering tables----+--
 CREATE TABLE public."ods_exams"
-("index" SERIAL PRIMARY KEY, "subject" VARCHAR(10) NOT NULL,"section" varchar(5) NOT NULL,"exam_date" DATE NOT NULL,
+("index" SERIAL PRIMARY KEY, "subject" VARCHAR(10) NOT NULL,"section" VARCHAR(5) NOT NULL,"exam_date" DATE NOT NULL,
  "proctor" VARCHAR(35) NOT NULL,"room_number" VARCHAR(50) NOT NULL,"start_time" TIMESTAMP NOT NULL,
  "end_time" TIMESTAMP NOT NULL,"actual_start" TIMESTAMP NOT NULL,"actual_end" TIMESTAMP NOT NULL,"first_entered" TIMESTAMP NOT NULL,
  "fileUploaded" VARCHAR(3),"received_as_paper_copy" VARCHAR(3) NOT NULL,"rescheduled" BIGINT NOT NULL,
@@ -47,7 +47,6 @@ ALTER TABLE ods_exams
 ----Add a new column to indicate Fall, Spring, or summer
 ALTER TABLE ods_exams
     ADD COLUMN semester CHAR(5);
-
 UPDATE ods_exams
     SET semester = CASE
         WHEN (exam_date BETWEEN '2019-05-01' AND '2019-08-03') THEN 'SU 19'
@@ -81,7 +80,6 @@ UPDATE ods_exams
 --Here I join two common table expressions(CTE) to query the number of exams for each 
 --date for both day and night time testing respectively
 --I wonder if there is a more efficient way to do this...
-
 --Fixed issue with nulls by using case statements to input 0 where there weren't any night exams
 CREATE VIEW ods_time_series_V02 AS(
 WITH nightExams AS (
@@ -105,14 +103,13 @@ GROUP BY dayExams.exam_date, dayExams.cnt_day_tests, nightExams.cnt_night_tests
 ORDER BY dayExams.exam_date ASC);
 
 ----Creating View of summary data for each semester
---No shows and exam_cancelleds removed
+--No shows and exam_cancelled instance filtered out
 --Note here that I use brackets to indicate that I am using the same query to create multiple views(i.e., fall, spring, ect)
-
 ----Time data grouping by course
 CREATE VIEW subject_time_data AS(
-    SELECT subject, semester, round(avg(allotted_time), 2) AS avg_allottment,
-       round(avg(actual_time), 2) AS avg_actual_time,
-       min(actual_time) AS min_actual_time, max(actual_time) AS max_actual_time
+    SELECT subject, semester, ROUND(avg(allotted_time), 2) AS avg_allottment,
+           ROUND(avg(actual_time), 2) AS avg_actual_time,
+       MIN(actual_time) AS min_actual_time, MAX(actual_time) AS max_actual_time
 FROM ods_exams
 WHERE actual_time <> 0
 GROUP BY subject, semester
@@ -139,7 +136,7 @@ CREATE VIEW reg_exams AS (
 ----Group by date for exams that were past 4:45 p.m. and aggregate the mean for allotted and actual time
 ----Filter out any 0 values for actual_time
 
-SELECT exam_date, round(avg(allotted_time), 0) AS avg_allottment, round(avg(actual_time), 0) AS avg_actual
+SELECT exam_date, ROUND(AVG(allotted_time), 0) AS avg_allottment, ROUND(AVG(actual_time), 0) AS avg_actual
 FROM ods_exams
 WHERE (CAST(start_time AS TIME) > '14:45:00') AND (actual_time <> 0) --to filter out no shows and exam cancelled
 GROUP BY exam_date
@@ -147,10 +144,10 @@ ORDER BY exam_date ASC;
 
 ----Average amount of time used per subject
 --Plan on joining this with "ods_time_series" view
-SELECT subject, round(avg(actual_time)) AS avg_time_used
+SELECT subject, ROUND(AVG((actual_time)) AS avg_time_used
 FROM ods_exams
 GROUP BY subject
-ORDER BY avg(actual_time) DESC;
+ORDER BY AVG(actual_time) DESC;
 
 ----Select Month, Day of Final Exams
 --There is only data for FAll 19 exams, and Spring 21 exams in the dataset
@@ -167,25 +164,27 @@ WHERE (EXTRACT (MONTH FROM exam_date) = 12 --Fall '19 Finals Month
 
 
 ----Summary data for regular semester exams grouped by semester
-SELECT semester, count(*) total_num_of_reg_exams, sum(allotted_time) as total_allotment_minutes_for_month, round(avg(allotted_time),2) as avg_allotment,
-       sum(actual_time) as total_actual_minutes_for_month, round(avg(actual_time),2) as avg_actual_time
+SELECT semester, COUNT(*) total_num_of_reg_exams, SUM(allotted_time) as total_allotment_minutes_for_month,
+       ROUND(AVG((allotted_time),2) as avg_allotment, SUM(actual_time) as total_actual_minutes_for_month,
+           ROUND(AVG(actual_time),2) as avg_actual_time
 FROM ods_exams
-WHERE actual_time <> 0 and final_exam = FALSE --Filter for regular semester exams and filter out no_shows, and cancelled
+WHERE actual_time <> 0 AND final_exam = FALSE --Filter for regular semester exams and filter out no_shows, and cancelled
 GROUP BY semester
 
 ----Summary data for regular semester exams grouped by semester
-SELECT semester, count(*) total_num_of_reg_exams, sum(allotted_time) as total_allotment_minutes_for_month, round(avg(allotted_time),2) as avg_allotment,
-       sum(actual_time) as total_actual_minutes_for_month, round(avg(actual_time),2) as avg_actual_time
+SELECT semester, COUNT(*) total_num_of_reg_exams, SUM(allotted_time) as total_allotment_minutes_for_month,
+           ROUND(AVG(allotted_time),2) as avg_allotment,
+           SUM(actual_time) as total_actual_minutes_for_month, ROUND(AVG(actual_time),2) as avg_actual_time
 FROM ods_exams
-WHERE actual_time <> 0 and final_exam = True --Filter for finals and filter out no_shows, and cancelled
+WHERE actual_time <> 0 AND final_exam = True --Filter for finals and filter out no_shows, and cancelled
 GROUP BY semester
 
 
 ----percent change in fields across semesters
 SELECT sp_20.month, sp_20.total_num_of_reg_exams AS sp_num_exams, sp_21.total_num_of_reg_exams AS sp_21_exams,
-       ROUND((sp_20.total_num_of_reg_exams - sp_21.total_num_of_reg_exams)::numeric / sp_21.total_num_of_reg_exams, 2) *100 AS percent_change
+       ROUND((sp_20.total_num_of_reg_exams - sp_21.total_num_of_reg_exams)::NUMERIC / sp_21.total_num_of_reg_exams, 2) *100 AS percent_change
 ---add in differences between allottment and time columns
 FROM sp_20
-JOIN sp_21 on
+JOIN sp_21 ON
 sp_20.month = sp_21.month
 
